7187194fd0f871b7f73d9d8cbe72650c08a3905e
Robert C. Taylor <r0wbrt@gmail.com>
2017-06-26 13:18:18 -0500

    Remove Old Tests, Delete OFDM support (Issue 19)

--- RadarTools/test/lib/YASDRR/DSP/buildtest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-Rscript spec-bottom-generator.r > spec.hs-bottom
-echo "-- This file was generated using buildtest.sh" > spec.hs
-cat spec.hs-top >> spec.hs
-cat spec.hs-bottom >> spec.hs
-
